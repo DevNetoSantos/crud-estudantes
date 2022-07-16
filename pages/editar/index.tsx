1cd82735c62bf661b4a078a622270ed7ef824326
DevNetoSantos <rasneto147@gmail.com>
2022-07-16 17:27:33 -0300

    home pronto

---
+++ pages/editar/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Editar.module.css';
+
+const Editar = () => {
+  return(
+    <div>
+      Editar
+    </div>
+  );
+}
+
+export default Editar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,57 @@
-const Home = () => {
+import styles from '../styles/Home.module.css';
+import api from '../services/api';
+import {User} from '../types/User';
+import Link from 'next/link';
+
+type Props = {
+  users: User[];
+}
+
+const Home = ({ users }: Props) => {
   return(
-    <div>
-      HOME
-    </div>
+    <div className={`${styles.containerHome}`}>
+        <table className={`table table-striped table-hover ${styles.table}`}>
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">City</th>
+            </tr>
+          </thead>
+          <tbody>
+              {users.map((item, index)=> (
+                <>
+                  <tr key={index}>
+                    <th scope="row">{item.id}</th>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.city}</td>
+                    <td>
+                      <button className='btn btn-danger'>Delete</button>
+                    </td>
+                    <td>
+                      <Link href={'/editar'}>
+                        <button className='btn btn-primary'>Edit</button>
+                      </Link>
+                    </td> 
+                  </tr>
+                </>
+              ))}
+          </tbody>
+        </table>
+      </div>
   );
+}
+
+export const getServerSideProps = async () => {
+  const users = await api.getAllUsers();
+
+  return {
+    props: {
+      users
+    }
+  }
 }
 
 export default Home;

e39ccac83aab6c6dbf02d18c95645ba0e9e56395
DevNetoSantos <rasneto147@gmail.com>
2022-07-16 15:35:24 -0300

    header feito responsivo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,40 @@
 import styles from '../../styles/Header.module.css';
+import Link from 'next/link';
+import {AiOutlineMenu} from 'react-icons/ai'
 
 const Header = () => {
   return(
-    <div>
-      Header
+    <div className={`container`}>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <Link className="navbar-brand" href="aaa">
+          <a>
+          <h1 className='display-8'>Crud</h1>
+          </a>
+        </Link>
+
+        <button 
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+            
+            <span> <AiOutlineMenu /> </span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className='navbar-nav ms-auto text-center'>
+            <li className='nav-item'>
+              <Link href='#'>
+                <button className='btn btn-primary'>Cadastrar</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
     </div>
   );
 }

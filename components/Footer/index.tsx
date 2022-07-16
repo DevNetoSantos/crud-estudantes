7d47e8bbdabd96fd09ddda946572efef5cd0e500
DevNetoSantos <rasneto147@gmail.com>
2022-07-16 15:49:09 -0300

    footer pronto

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,8 +2,8 @@ import styles from '../../styles/Footer.module.css';
 
 const Footer = () => {
   return(
-    <div>
-      Footer
+    <div className={`container bg-light ${styles.containerFooter}`}>
+      <h5 className='display-8'>Projeto feito para praticas de estudos</h5>
     </div>
   );
 }
